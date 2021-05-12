8757cf196db974eddbd2c30604652148d6a3f927
dream-huan <52626649+dream-huan@users.noreply.github.com>
2021-05-12 04:01:30 +0000

    Enter

--- mainproject/go/project/gologger/gologger.go
+++ mainproject/go/project/gologger/gologger.go
@@ -1,26 +1,31 @@
 package gologger
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"strconv"
 	"time"
 )
 
-func Logwrite(err string) {
+func Logwrite(str string) {
 	file, err := os.OpenFile("../log/log error"+time.Now().Format("2006-01-02")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		Logwrite(err)
+		_, file, line, ok := runtime.Caller(1)
+		Logwrite(fmt.Sprintf("%v", err) + " " + file + " " + strconv.Itoa(line) + strconv.FormatBool(ok))
 	}
 	log.SetOutput(file)
 	log.SetPrefix("[Error]")
 	log.SetFlags(log.Ldate)
-	log.Printf("%#v", err)
+	log.Printf("%#v", str)
 }
 
 func BasicLogwrite(msg string) {
 	file, err := os.OpenFile("../log/log basic"+time.Now().Format("2006-01-02")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		Logwrite(err)
+		_, file, line, ok := runtime.Caller(1)
+		Logwrite(fmt.Sprintf("%v", err) + " " + file + " " + strconv.Itoa(line) + strconv.FormatBool(ok))
 	}
 	log.SetOutput(file)
 	log.SetFlags(log.Ldate)
